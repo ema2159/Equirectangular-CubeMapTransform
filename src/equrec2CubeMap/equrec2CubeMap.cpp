f2d7391c30f13b6d53bbc39c1c1572aa86027663
Emmanuel Bustos <ema2159@gmail.com>
2021-04-28 13:10:24 +0200

    Add: Equirectangular to cubemap implementation

---
+++ src/equrec2CubeMap/equrec2CubeMap.cpp
@@ -0,0 +1,149 @@
+#include <chrono> // for high_resolution_clock
+#include <iostream>
+#include <math.h> /* modf */
+#include <omp.h>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+float getTheta(float x, float y) {
+  float rtn = 0;
+  if (y < 0) {
+    rtn = atan2(y, x) * -1;
+  } else {
+    rtn = M_PI + (M_PI - atan2(y, x));
+  }
+  return rtn;
+}
+
+int main(int argc, char **argv) {
+
+  // Get images from command line
+  cv::Mat source = cv::imread(argv[1], cv::IMREAD_COLOR);
+
+  // Get operation parameters from the command line
+  float scaleX = min(atof(argv[2]), 5.0);
+  float scaleY = min(atof(argv[3]), 5.0);
+
+  // Create placeholder windows
+  cv::namedWindow("Original Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
+  cv::namedWindow("Processed Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
+
+  cv::imshow("Original Image", source);
+
+  const int inputWidth = source.cols;
+  const int inputHeight = source.rows;
+  const int sqr = source.cols / 4.0;
+  const int output_width = sqr * 3;
+  const int output_height = sqr * 2;
+
+  // Placeholder image for the result
+  cv::Mat destination(output_height, output_width, CV_8UC3,
+                      cv::Scalar(255, 255, 255));
+
+  auto begin = chrono::high_resolution_clock::now();
+
+  const int iter = 1;
+  for (int it = 0; it < iter; it++) {
+#pragma omp parallel for
+    for (int j = 1; j < destination.cols; j++) {
+      // #pragma omp parallel for
+      for (int i = 1; i < destination.rows; i++) {
+        float tx = 0.0;
+        float ty = 0.0;
+        float x = 0.0;
+        float y = 0.0;
+        float z = 0.0;
+
+        if (i < sqr + 1) {   // top half
+          if (j < sqr + 1) { // top left box [Y+]
+            tx = j;
+            ty = i;
+            x = tx - 0.5 * sqr;
+            y = 0.5 * sqr;
+            z = ty - 0.5 * sqr;
+          } else if (j < 2 * sqr + 1) { // top middle [X+]
+            tx = j - sqr;
+            ty = i;
+            x = 0.5 * sqr;
+            y = (tx - 0.5 * sqr) * -1;
+            z = ty - 0.5 * sqr;
+          }
+
+          else { // top right [Y-]
+            tx = j - sqr * 2;
+            ty = i;
+            x = (tx - 0.5 * sqr) * -1;
+            y = -0.5 * sqr;
+            z = ty - 0.5 * sqr;
+          }
+        } else {                 // bottom half
+          if (j < sqr + 1) { // bottom left box [X-]
+
+            tx = j;
+            ty = i - sqr;
+            x = int(-0.5 * sqr);
+            y = int(tx - 0.5 * sqr);
+            z = int(ty - 0.5 * sqr);
+          }
+
+          // print(str(x)+", "+str(y)+", "+str(z))
+
+          else if (j < 2 * sqr + 1) { // bottom middle [Z-]
+
+            tx = j - sqr;
+            ty = i - sqr;
+            x = (ty - 0.5 * sqr) * -1;
+            y = (tx - 0.5 * sqr) * -1;
+            z = 0.5 * sqr; // was -0.5 might be due to phi being reversed
+          }
+
+          else { // bottom right [Z+]
+
+            tx = j - sqr * 2;
+            ty = i - sqr;
+            x = ty - 0.5 * sqr;
+            y = (tx - 0.5 * sqr) * -1;
+            z = -0.5 * sqr; // was +0.5 might be due to phi being reversed
+          }
+        }
+
+        // now find out the polar coordinates
+
+        float rho = sqrt(x * x + y * y + z * z);
+        float normTheta =
+            getTheta(x, y) / (2 * M_PI); // /(2*M_PI) normalise theta
+        float normPhi = (M_PI - acos(z / rho)) / M_PI; // /M_PI normalise phi
+
+        // use this for coordinates
+
+        float iX = normTheta * inputWidth;
+        float iY = normPhi * inputHeight;
+
+        // catch possible overflows
+
+        if (iX >= inputWidth) {
+          iX = iX - (inputWidth);
+        }
+        if (iY >= inputHeight) {
+          iY = iY - (inputHeight);
+        }
+
+        // cout << int(iX) << endl;
+        // cout << iY << endl;
+        destination.at<cv::Vec3b>(i, j) = source.at<cv::Vec3b>(int(iY), int(iX));
+      }
+    }
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = end - begin;
+
+  cv::imshow("Processed Image", destination);
+
+  cout << "Processing time: " << diff.count() << " s" << endl;
+  cout << "Time for 1 iteration: " << diff.count() / iter << " s" << endl;
+  cout << "IPS: " << iter / diff.count() << endl;
+
+  cv::waitKey();
+  return 0;
+}
