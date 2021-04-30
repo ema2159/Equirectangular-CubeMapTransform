d87bf5858a1b2f5d8cab7b2cdcfa5846ad7dd7e7
Emmanuel Bustos <ema2159@gmail.com>
2021-04-30 11:39:09 +0200

    Clean: Format file

--- src/cubeMap2EqurecCUDA/cubeMap2Equrec.cpp
+++ src/cubeMap2EqurecCUDA/cubeMap2Equrec.cpp
@@ -7,14 +7,14 @@
 using namespace std;
 
 void startCUDA(cv::cuda::GpuMat &posY, cv::cuda::GpuMat &posX,
-	       cv::cuda::GpuMat &negY, cv::cuda::GpuMat &negX,
-	       cv::cuda::GpuMat &negZ, cv::cuda::GpuMat &posZ,
-	       cv::cuda::GpuMat &dst);
+               cv::cuda::GpuMat &negY, cv::cuda::GpuMat &negX,
+               cv::cuda::GpuMat &negZ, cv::cuda::GpuMat &posZ,
+               cv::cuda::GpuMat &dst);
 
 int main(int argc, char **argv) {
   cv::namedWindow("Processed Image", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
 
-  cv::cuda::GpuMat d_result, posY, posX, negY, negX, posZ, negZ; 
+  cv::cuda::GpuMat d_result, posY, posX, negY, negX, posZ, negZ;
 
   // Get six cube images inside a given directory
   std::string imgs_path(argv[1]);
@@ -32,19 +32,29 @@ int main(int argc, char **argv) {
   // 	| X- | Z- | Z+ |
   // 	+----+----+----+
   // cv::Mat h_img = cv::imread(argv[1]);
-  // posY.upload(h_img(cv::Rect(0, 0, h_img.cols/3, h_img.rows/2)));
-  // posX.upload(h_img(cv::Rect(h_img.cols/3, 0, h_img.cols/3, h_img.rows/2)));
-  // negY.upload(h_img(cv::Rect(2*h_img.cols/3, 0, h_img.cols/3, h_img.rows/2)));
-  // negX.upload(h_img(cv::Rect(0, h_img.rows/2, h_img.cols/3, h_img.rows/2)));
-  // negZ.upload(h_img(cv::Rect(h_img.cols/3, h_img.rows/2, h_img.cols/3, h_img.rows/2)));
-  // posZ.upload(h_img(cv::Rect(2*h_img.cols/3, h_img.rows/2, h_img.cols/3, h_img.rows/2)));
-  // Write individual extracted images
-  // cv::imwrite("posy.jpg", h_img(cv::Rect(0, 0, h_img.cols/3, h_img.rows/2)));
-  // cv::imwrite("posx.jpg", h_img(cv::Rect(h_img.cols/3, 0, h_img.cols/3, h_img.rows/2)));
-  // cv::imwrite("negy.jpg", h_img(cv::Rect(2*h_img.cols/3, 0, h_img.cols/3, h_img.rows/2)));
-  // cv::imwrite("negx.jpg", h_img(cv::Rect(0, h_img.rows/2, h_img.cols/3, h_img.rows/2)));
-  // cv::imwrite("negz.jpg", h_img(cv::Rect(h_img.cols/3, h_img.rows/2, h_img.cols/3, h_img.rows/2)));
-  // cv::imwrite("posz.jpg", h_img(cv::Rect(2*h_img.cols/3, h_img.rows/2, h_img.cols/3, h_img.rows/2)));
+  // posY.upload(h_img(cv::Rect(0, 0, h_img.cols / 3, h_img.rows / 2)));
+  // posX.upload(
+  //     h_img(cv::Rect(h_img.cols / 3, 0, h_img.cols / 3, h_img.rows / 2)));
+  // negY.upload(
+  //     h_img(cv::Rect(2 * h_img.cols / 3, 0, h_img.cols / 3, h_img.rows / 2)));
+  // negX.upload(
+  //     h_img(cv::Rect(0, h_img.rows / 2, h_img.cols / 3, h_img.rows / 2)));
+  // negZ.upload(h_img(cv::Rect(h_img.cols / 3, h_img.rows / 2, h_img.cols / 3,
+  //                            h_img.rows / 2)));
+  // posZ.upload(h_img(cv::Rect(2 * h_img.cols / 3, h_img.rows / 2, h_img.cols / 3,
+  //                            h_img.rows / 2)));
+  // Write individual extracted images cv::imwrite(
+  //     "posy.jpg", h_img(cv::Rect(0, 0, h_img.cols / 3, h_img.rows / 2)));
+  // cv::imwrite("posx.jpg", h_img(cv::Rect(h_img.cols / 3, 0, h_img.cols / 3,
+  //                                        h_img.rows / 2)));
+  // cv::imwrite("negy.jpg", h_img(cv::Rect(2 * h_img.cols / 3, 0, h_img.cols / 3,
+  //                                        h_img.rows / 2)));
+  // cv::imwrite("negx.jpg", h_img(cv::Rect(0, h_img.rows / 2, h_img.cols / 3,
+  //                                        h_img.rows / 2)));
+  // cv::imwrite("negz.jpg", h_img(cv::Rect(h_img.cols / 3, h_img.rows / 2,
+  //                                        h_img.cols / 3, h_img.rows / 2)));
+  // cv::imwrite("posz.jpg", h_img(cv::Rect(2 * h_img.cols / 3, h_img.rows / 2,
+  //                                        h_img.cols / 3, h_img.rows / 2)));
 
   const int output_width = posY.rows * 2;
   const int output_height = posY.rows;
